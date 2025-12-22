a688f5bf043f74588a1103716d6b87fc7386076c
Artem Alexeyenko <ala@sitecore.net>
2025-12-22 17:45:32 -0500

    finalize changesets move?

--- .beachball/beach-utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ChangelogEntry } from 'beachball';
-
-export const changelog = {
-  renderEntry: (entry: ChangelogEntry) => {
-    return `[${entry.package}] ${entry.comment}`;
-  },
-};
